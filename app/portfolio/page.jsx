dfacf720f683c6e6ec5d35c852de4ef0776254ef
SJ <127341132+minari314159@users.noreply.github.com>
2024-07-30 15:54:36 -0400

    commit

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useRef } from "react";
 import { web_projects } from "@/app/data";
+import Link from "next/link";
 import { motion, useScroll, useSpring } from "framer-motion";
 import SingleProject from "./SingleProject";
+import { Inika } from "next/font/google";
 
 const Projects = () => {
 	const ref = useRef();
@@ -24,13 +26,40 @@ const Projects = () => {
 				</h1>
 				<motion.div
 					style={{ scaleX }}
-					transition={{delay:0.5}}
+					transition={{ delay: 0.5 }}
 					className="h-1 bg-textmain backdrop-filter backdrop-blur-md bg-opacity-50  shadow-xl rounded-full"></motion.div>
 			</div>
 			<div className="h-full">
 				{web_projects.map((project, index) => (
 					<SingleProject project={project} key={index} />
 				))}
+			</div>
+			<div className="w-screen h-screen flex flex-col items-center justify-center text-center">
+				<h1 className="text-5xl">Do you have a project?</h1>
+				<div className="relative">
+					<motion.svg
+						animate={{ rotate: 360 }}
+						transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+						viewBox="0 0 300 300"
+						className="w-[300px] h-[300px] md:w-[500px] md:h-[500px]">
+						<defs>
+							<path
+								id="circlePath"
+								d="M 150, 150 m -60, 0 a 60, 60 0 0, 1 120, 0 a 60, 60 0 0, 1 -120, 0"
+							/>
+						</defs>
+						<text fill="#ffffff">
+							<textPath xlinkHref="#circlePath" className="text-2xl">
+								Full Stack Developer UI Designer
+							</textPath>
+						</text>
+					</motion.svg>
+					<Link
+						href="/contact"
+						className="w-16 h-16 md:w-28 md:h-28 top-0 left-0 right-0 bottom-0 m-auto bg-black text-white rounded-full absolute flex items-center justify-center z-50">
+						Hire
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
