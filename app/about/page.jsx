c672e0276089ee0584e94d8f15cd0edd2767c395
SJ <127341132+minari314159@users.noreply.github.com>
2024-10-29 15:05:48 -0400

    IMproved about page description

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -12,19 +12,6 @@ const Services = () => {
 			className=" w-full max-w-[1336px] min-h-[100vh] flex flex-col items-center justify-between gap-5 px-5 overflow-hidden"
 			variants={serviceVariants}>
 			<motion.div
-				className="flex-1  self-end flex items-center justify-end w-full "
-				variants={serviceVariants}
-				initial={serviceVariants.initial}
-				animate={serviceVariants.animate}>
-				<p className="text-textmain text-right text-sm md:text-md ">
-					<i>
-						An engineer and designer specializing in <br /> sustainable material
-						design and <br /> computational design.
-					</i>
-				</p>
-				<hr className=" w-[20%]  border-t-textmain ml-2" />
-			</motion.div>
-			<motion.div
 				className="flex-1 flex flex-col items-center justify-center gap-5 w-full"
 				variants={serviceVariants}
 				initial={serviceVariants.initial}
@@ -48,18 +35,25 @@ const Services = () => {
 				width={320}
 				height={20}
 				alt="Divider"
-				className="object-cover opacity-5"
+				className="object-cover opacity-10"
 			/>
 			<div className="w-[80%] p-2 flex flex-col items-center gap-5">
 				<h1 className="text-4xl md:text-5xl  ">The Approach</h1>
 				<p className="text-center">
-					The edge between scientist and designer may appear to be opposing
-					however it is finding that bridge between the two which creates a
-					philosphy of relishing in the ambiguous edges of the natural and
-					synthetic world.The approach entails three pillars, which can then be
-					re-evaluated until the desired function is achieved. It is applying
-					the scientific method to the design process, which creates ambiguous
-					edges between the two worlds pushing for nature centric design.
+					As a professional with a background in physics and chemistry, I bring
+					a unique perspective to machine learning engineering and data
+					analysis. My experience in these scientific disciplines has honed my
+					analytical skills and cultivated a rigorous, quantitative approach to
+					problem-solving, which I now apply to data-driven projects. Leveraging
+					the mathematical foundations from physics, I approach machine learning
+					with a focus on precision, model optimization, and a deep
+					understanding of underlying principles. This scientific mindset,
+					combined with technical expertise in data analysis, allows me to craft
+					robust models that not only capture complex patterns but also deliver
+					actionable insights for diverse fields, including environmental
+					science, healthcare, and sustainability. My work aims to bridge the
+					gap between theory and application, transforming raw data into
+					impactful solutions.
 				</p>
 			</div>
 
